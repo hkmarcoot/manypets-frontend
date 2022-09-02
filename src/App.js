7edc50ba1cf1e284f91c7b71397dac1fb1d8787f
Jordan Flash <flash.jordan.3@gmail.com>
2022-09-02 15:15:50 +0100

    minor css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import "./index.css";
 
 const API_URL = "http://localhost:3000";
 
@@ -8,7 +9,6 @@ function App() {
   const [error, setError] = useState(null);
   const [status, setStatus] = useState("");
   const [isPending, setIsPending] = useState(true);
-
 
   const [formFields, setFormFields] = useState({
     petsname: "",
@@ -19,20 +19,20 @@ function App() {
     age: 0,
     address: "",
     email: "",
-  })
+  });
 
-  const [quote, setQuote] = useState(0)
+  const [quote, setQuote] = useState(0);
 
-  const [Id, setId] = useState("")
+  const [Id, setId] = useState("");
 
-  function handleChange(event){
-        let value = (event.target.value)
-        setFormFields({ ...formFields, [event.target.name]: value });
+  function handleChange(event) {
+    let value = event.target.value;
+    setFormFields({ ...formFields, [event.target.name]: value });
   }
 
-  function handleId(event){
-        let value = (event.target.value)
-        setId( value );
+  function handleId(event) {
+    let value = event.target.value;
+    setId(value);
   }
 
   async function isBreedValid(species, breed) {
@@ -42,64 +42,62 @@ function App() {
     } else if (species === "dog") {
       api = "https://api.thedogapi.com/v1/breeds";
     }
-  
+
     console.log(api + "/search?q=" + breed.replace(" ", "_"));
     const response = await fetch(api + "/search?q=" + breed.replace(" ", "_"));
     const payload = await response.json();
-  
+
     return payload.length; // update this later -> if length is 0, say no breed found, if length is > 1, say be more specific
   }
 
-
   async function getQuote() {
     let response = await fetch(`http://localhost:3000/quotes/${Id}`);
-    let data = await response.json()
-    setQuote(data.payload)
+    let data = await response.json();
+    setQuote(data.payload);
   }
 
-  async function onSubmit(){
+  async function onSubmit() {
     // add in validation
     // for (let i = 0; i < pets.length; i++) {
-    //   // check breed is valid, if it 
+    //   // check breed is valid, if it
     //   const results = isBreedValid(pets[i].species, pets[i].breed)
-  
+
     //   if (results === 0) {
     //     res.json({ success: false, message: "Invalid breed - none found."})
     //     return;
     //   }
-  
+
     //   if (results > 1) {
     //     res.json({ success: false, message: "Multiple breed founds - please be more specific."})
     //     return;
     //   }
     // }
-    const results = await isBreedValid(formFields.species, formFields.breed)
-  
-      if (results === 0) {
-        alert("Invalid breed - none found.")
-        
-        return;
-      }
-  
-      if (results > 1) {
-        alert("Multiple breed founds - please be more specific.")
-        // res.json({ success: false, message: "Multiple breed founds - please be more specific."})
-        return;
-      }
-    
-      
-  //add validation on the address
+    const results = await isBreedValid(formFields.species, formFields.breed);
 
-  // send the post request
-  const response = await fetch('http://localhost:3000/quotes', {
-    headers: {
-      "content-type": "application/json"
-    },
-    method: "POST",
-    body: JSON.stringify(formFields)
-  })
+    if (results === 0) {
+      alert("Invalid breed - none found.");
 
-  alert("Form Submitted")
+      return;
+    }
+
+    if (results > 1) {
+      alert("Multiple breed founds - please be more specific.");
+      // res.json({ success: false, message: "Multiple breed founds - please be more specific."})
+      return;
+    }
+
+    //add validation on the address
+
+    // send the post request
+    const response = await fetch("http://localhost:3000/quotes", {
+      headers: {
+        "content-type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(formFields),
+    });
+
+    alert("Form Submitted");
   }
   useEffect(() => {
     async function getnumofquote() {
@@ -135,27 +133,74 @@ function App() {
         </div>
         <div className="form">
           <label>name</label>
-          <input type="text" name="name" value={formFields.name} onChange={handleChange}></input>
+          <input
+            type="text"
+            name="name"
+            value={formFields.name}
+            onChange={handleChange}
+          ></input>
+          <br></br>
           <label>gender</label>
-          <input type="text"  name="gender" value={formFields.gender} onChange={handleChange}></input>
+          <input
+            type="text"
+            name="gender"
+            value={formFields.gender}
+            onChange={handleChange}
+          ></input>
+          <br></br>
           <label>email</label>
-          <input type="text"  name="email" value={formFields.email} onChange={handleChange}></input>
+          <input
+            type="text"
+            name="email"
+            value={formFields.email}
+            onChange={handleChange}
+          ></input>
+          <br></br>
           <label>species</label>
-          <input type="text"  name="species" value={formFields.species} onChange={handleChange}></input>
+          <input
+            type="text"
+            name="species"
+            value={formFields.species}
+            onChange={handleChange}
+          ></input>
+          <br></br>
           <label>breed type</label>
-          <input type="text"  name="breedType" value={formFields.breedType} onChange={handleChange}></input>
+          <input
+            type="text"
+            name="breedType"
+            value={formFields.breedType}
+            onChange={handleChange}
+          ></input>
+          <br></br>
           <label>breed</label>
-          <input type="text"  name="breed" value={formFields.breed} onChange={handleChange}></input>
+          <input
+            type="text"
+            name="breed"
+            value={formFields.breed}
+            onChange={handleChange}
+          ></input>
+          <br></br>
           <label>age (months)</label>
-          <input type="text"  name="age" value={formFields.age} onChange={handleChange}></input>
+          <input
+            type="text"
+            name="age"
+            value={formFields.age}
+            onChange={handleChange}
+          ></input>
+          <br></br>
           <label>address</label>
-          <input type="text"  name="address" value={formFields.address} onChange={handleChange}></input>
+          <input
+            type="text"
+            name="address"
+            value={formFields.address}
+            onChange={handleChange}
+          ></input>
         </div>
         <button onClick={onSubmit}>submit</button>
       </div>
       <div>
         <label>User ID</label>
-        <input type="text"  name="Id" value={Id} onChange={handleId}></input>
+        <input type="text" name="Id" value={Id} onChange={handleId}></input>
         <button onClick={getQuote}>Get Quote</button>
         <h2>Yearly cost for your pets: {quote}</h2>
       </div>
